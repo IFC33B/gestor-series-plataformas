7a4af223c785aa96933ba1458e8c0d70ca0da3c0
Cristian Pérez Juárez <cristianpj0808@gmail.com>
2025-12-29 17:58:17 +0100

    Create: Initial GestorController.java

---
+++ back-end/gestor-series-plataformes/src/main/java/ifc33b/dwesc/gestor_series_plataformes/controller/GestorController.java
@@ -0,0 +1,36 @@
+package ifc33b.dwesc.gestor_series_plataformes.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.validation.Valid;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
+
+@CrossOrigin(origins = "http://localhost:4200")
+@RestController
+@RequestMapping("/api")
+public class GestorController {
+
+    @GetMapping("/plataformes") // Devuelve las plataformas
+    public int getPlataformes() {
+        return 0;
+    }
+    
+    @GetMapping("/series/plataforma/{id}") // Devuelve las series de una plataforma
+    public int getSeries(@PathVariable Long id) {
+        return 0;
+    }
+    
+    @PostMapping("/series") // Crea una nueva serie
+    public int postMethodName(@Valid @RequestBody String entity) {        
+        return 0;
+    }
+    
+}
