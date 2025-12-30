c4245e3f0c532cdfd72a424b0e8155cf893b2f63
Cristian Pérez Juárez <cristianpj0808@gmail.com>
2025-12-30 16:26:48 +0100

    Update(GestorController): all methods

--- back-end/gestor-series-plataformes/src/main/java/ifc33b/dwesc/gestor_series_plataformes/controller/GestorController.java
+++ back-end/gestor-series-plataformes/src/main/java/ifc33b/dwesc/gestor_series_plataformes/controller/GestorController.java
@@ -1,8 +1,18 @@
 package ifc33b.dwesc.gestor_series_plataformes.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import ifc33b.dwesc.gestor_series_plataformes.dto.PlataformaResponse;
+import ifc33b.dwesc.gestor_series_plataformes.dto.SerieRequest;
+import ifc33b.dwesc.gestor_series_plataformes.dto.SerieResponse;
+import ifc33b.dwesc.gestor_series_plataformes.service.GestorService;
 import jakarta.validation.Valid;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,20 +23,34 @@ import org.springframework.web.bind.annotation.RequestBody;
 @RestController
 @RequestMapping("/api")
 public class GestorController {
+    @Autowired
+    private GestorService gestorService;
 
     @GetMapping("/plataformes") // Devuelve las plataformas
-    public int getPlataformes() {
-        return 0;
+    public ResponseEntity<List<PlataformaResponse>> getPlataformes() {
+        // Service
+        List<PlataformaResponse> response = gestorService.getPlataformes();
+
+        // HTTP Response
+        return ResponseEntity.ok(response);
     }
 
     @GetMapping("/series/plataforma/{id}") // Devuelve las series de una plataforma
-    public int getSeries(@PathVariable Long id) {
-        return 0;
+    public ResponseEntity<List<SerieResponse>> getSeries(@PathVariable Long id) {
+        // Service
+        List<SerieResponse> response = gestorService.getSeries(id);
+
+        // HTTP Response
+        return ResponseEntity.ok(response);
     }
 
     @PostMapping("/series") // Crea una nueva serie
-    public int createSerie(@Valid @RequestBody String entity) {
-        return 0;
+    public ResponseEntity<SerieResponse> createSerie(@Valid @RequestBody SerieRequest request) {
+        // Service
+        SerieResponse response = gestorService.createSerie(request);
+
+        // HTTP Response
+        return new ResponseEntity<>(response, HttpStatus.CREATED);
     }
 
 }
